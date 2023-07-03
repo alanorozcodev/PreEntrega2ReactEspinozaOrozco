5c5aec091eb199394a7f38172afb4ece0318a62d
Alan Espinoza <alan.orozco43@gmail.com>
2023-07-02 18:29:40 -0600

    se agrega react bootstrap para darle un poco de estilo a los botones

--- sharktcg/src/components/NavBar/NavBar.js
+++ sharktcg/src/components/NavBar/NavBar.js
@@ -1,17 +1,18 @@
 import CartWidget from "../CartWidget/CartWidget";
 import LogoSharkTCG from '../NavBar/assets/logo/LogoSharkTCG.png';
+import Button from 'react-bootstrap/Button';
 
 const NavBar = () => {
     return (
         <nav>
             <img src={LogoSharkTCG} alt= "Logo Shark TCG" />
             <div>
-                <button>Preventas</button>
-                <button>Juego de Cartas</button>
-                <button>Accesorios</button>
-                <button>Mayoreo</button>
-                <button>Ubicacion</button>
-                <button>Contacto</button>
+                <Button variant="outline-primary">Preventas</Button>
+                <Button variant="outline-primary">Juego de Cartas</Button>
+                <Button variant="outline-primary">Accesorios</Button>
+                <Button variant="outline-primary">Mayoreo</Button>
+                <Button variant="outline-primary">Ubicacion</Button>
+                <Button variant="outline-primary">Contacto</Button>
             </div>
             <CartWidget />
         </nav>
